410145a955da7c3943362c41481e52a350884623
andikk <andikk@mail.ru>
2019-10-25 21:36:13 +0300

    module2-task1

--- index.php
+++ index.php
@@ -65,17 +65,13 @@ $lots = [
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
             <?php if ($is_auth): ?>
-
                 <div class="user-menu__logged">
                     <p><?= $user_name ?></p>
                     <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                     <a class="user-menu__logout" href="#">Выход</a>
                 </div>
-
             <?php else: ?>
-
                 <ul class="user-menu__list">
                     <li class="user-menu__item">
                         <a href="#">Регистрация</a>
@@ -84,9 +80,7 @@ $lots = [
                         <a href="#">Вход</a>
                     </li>
                 </ul>
-
             <?php endif ?>
-
         </nav>
     </div>
 </header>
