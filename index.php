fa8acad4354f5d9d5df58aa6cadeed9206646031
andikk <andikk@mail.ru>
2019-10-25 21:20:20 +0300

    module2-task1

--- index.php
+++ index.php
@@ -108,18 +108,19 @@ $lots = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <!--заполните этот список из массива с товарами-->
             <?php foreach ($lots as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= $lot['url'] ?>" width="350" height="260" alt="">
+                        <img src="" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?= $lot['cat'] ?></span>
                         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot['name'] ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
-                                <span class="lot__amount"><?= $lot['price'] ?></span>
-                                <span class="lot__cost">цена<b class="rub">р</b></span>
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?= $lot['price'] ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
