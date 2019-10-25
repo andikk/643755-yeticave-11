2067daeae65f18c53d8d9a0812c1d2f95e2fcc2b
andikk <andikk@mail.ru>
2019-10-25 21:21:23 +0300

    module2-task1

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ $lots = [
             <?php foreach ($lots as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="" width="350" height="260" alt="">
+                        <img src="<?= $lot['url'] ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?= $lot['cat'] ?></span>
