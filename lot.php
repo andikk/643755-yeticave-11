645226eac1edf4e22889db51d31dbf9305b26327
andikk <andikk@mail.ru>
2019-10-31 19:55:42 +0300

    module6-task1

--- lot.php
+++ lot.php
@@ -15,17 +15,19 @@ $lot = findOne($sqlLot, $link);
 if ($lot === null) {
     http_response_code(404);
     $page_content = include_template('error.php', ['error' => 'Даннай лот не найден']);
+    $page_title = "Даннай лот не найден";
 } else {
     $page_content = include_template('lot.php', ['categories' => $categories,
         'lot' => $lot,
         'expiryTime' => get_dt_range($lot['expiry_date'])]);
+    $page_title = $lot['name'];
 }
 
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
     'categories' => $categories,
-    'title' => esc($lot['name']),
+    'title' => $page_title,
     'is_auth' => $is_auth,
     'user_name' => $user_name
 ]);
